9ab42ea8dc88e18943a1c46e399c725fd8b4d5ae
fabricem15 <97371239+fabricem15@users.noreply.github.com>
2023-12-10 21:50:49 -0500

    Update sql statements.sql

--- sql statements.sql
+++ sql statements.sql
@@ -13,10 +13,12 @@ select * from classes
 natural join bookings
 where bookings.day_of_week='Tuesday'
 
+
 -- 4. update member profile for Bob Williams after a 10kg weight loss 
 UPDATE members 
 SET weight = members.weight - 10 
 where members.first_name = 'Bob' AND members.last_name='Williams'
+
 
 -- 5. Get the member with the most workouts
 SELECT members.*, COUNT(*) as num_workouts 
@@ -24,27 +26,29 @@ from members natural join workout_history
 group by member_id 
 order by num_workouts desc limit 1
 
--- 5. add new workout history for Jane Smith who is a gym rat and loves HIIT
+
+-- 6. add new workout history for Jane Smith who is a gym rat and loves HIIT
 INSERT into workout_history (member_id, workout_id) 
 values ((select member_id from members where first_name='Jane'and last_name='Smith'),
-		(select workout_id from workouts where workout_name='HIIT Workout'));
-		
--- 6. Add new member goal
+        (select workout_id from workouts where workout_name='HIIT Workout'));
+        
+
+-- 7. Add new member goal
 INSERT INTO fitness_goals (member_id, goal_name, goal_description, goal_type_id, target_value, start_date, end_date, achieved)
 VALUES (2, 'Improve Running Pace', 'Training to reduce running time for a 5K race', 3, NULL, '2023-11-01', '2023-12-31', FALSE)
 
 
--- 7. Delete the Taekwando class -> deletes every booking that references it 
+-- 8. Delete the Taekwando class -> deletes every booking that references it 
 DELETE FROM classes where class_name = 'Taekwando'
 
--- 8. Insert class notes for trainer with id 3
+
+-- 9. Insert class notes for trainer with id 3
 INSERT INTO class_history (trainer_id, class_id, notes, attendance) values (3, 2, 'Great class today! Focused on core exercises and cardio. Encouraged proper form throughout the session.', 25) 
 
-
--- 9. Update a class by changing the trainer
-UPDATE classes where class_id = 1 
+-- 10. Update a class by changing the trainer
+UPDATE classes 
 SET trainer_id = 7
+where class_id = 1 
 
-
--- 10. Delete injury rehabilitation goal type 
+-- 11. Delete injury rehabilitation goal type 
 DELETE FROM goal_type where goal_title = 'Injury Rehabilitation'
